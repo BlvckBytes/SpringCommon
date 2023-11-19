a517d479ef96a7bec4527ef7fd3b530c01774e29
Philip Hell <blvckbytes@gmail.com>
2023-11-19 19:40:13 +0100

    added DescribedException#fromDescription

--- src/main/kotlin/me/blvckbytes/springcommon/exception/DescribedException.kt
+++ src/main/kotlin/me/blvckbytes/springcommon/exception/DescribedException.kt
@@ -2,6 +2,16 @@ package me.blvckbytes.springcommon.exception
 
 abstract class DescribedException : RuntimeException() {
 
+  companion object {
+    fun fromDescription(description: String): DescribedException {
+      return object : DescribedException() {
+        override fun getDescription(): String {
+          return description
+        }
+      }
+    }
+  }
+
   abstract fun getDescription(): String
 
 }
